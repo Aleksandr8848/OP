1638631913e6597b4b91dc23eb098c3d71784048
LebedevAA <lebedevaleksandr8848@gmail.com>
2022-02-16 00:01:31 +0300

    second task

--- main.c
+++ main.c
@@ -22,15 +22,48 @@ void test_swapRowsWithMaxAndMinElements() {
             1, 2, 3
     }, 3, 3);
 
-    assert(twoMatricesEqual(m,x));
+    assert(twoMatricesEqual(m, x));
     freeMemMatrix(m);
     freeMemMatrix(x);
 }
 
-void test(){
-    test_swapRowsWithMaxAndMinElements;
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > a[max])
+            max = a[i];
+    }
+    return max;
 }
+
+void sortRowsByMinElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+
+void test_sortRowsByMinElement(){
+    matrix m = createMatrixFromArray((int[]) {
+            4, 5, 9,
+            6, 7, 8,
+            1, 2, 3
+    }, 3, 3);
+
+    sortRowsByMinElement(m);
+    matrix x = createMatrixFromArray((int[]) {
+            1, 2, 3,
+            4, 5, 9,
+            6, 7, 8
+    }, 3, 3);
+    assert(twoMatricesEqual(m, x));
+    freeMemMatrix(m);
+    freeMemMatrix(x);
+}
+
+void test() {
+    test_swapRowsWithMaxAndMinElements;
+    test_sortRowsByMinElement;
+}
+
 int main() {
-test();
+    test();
     return 0;
 }
